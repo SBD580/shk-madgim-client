4febd5135f521514d1a396cc82ea64c903255d14
yardenDavidof <Yarden1993>
2016-08-08 17:09:21 +0300

    update d3 data dynamiclly

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -95,7 +95,7 @@ var container = map.getCanvasContainer();
 var svg = d3.select(container).append("svg")
 // var transform = d3.geo.transform({point: projectPoint});
 // var path = d3.geo.path().projection(transform);
-var plain;
+var plainGroup;
 var plainsJSON;
 var plainController = [];
 var currentTimestamp;
@@ -151,7 +151,7 @@ function initPlayer() {
             updateDateUI(ui.value);
         },
         stop: function (event, ui) {
-          console.log('slider released');
+            console.log('slider released');
             updateData();
         }
     });
@@ -228,7 +228,6 @@ function initPlayer() {
 
 };
 
-
 function updateDateUI(timestamp) {
     $('#cursor-date').html(dateStr(timestamp));
     $('#cursor-time').html(timeStr(timestamp));
@@ -240,7 +239,7 @@ function setSliderMove(timestamp) {
     updateProjection();
 }
 
-function getNextPos(timestamp, plainTicks){
+function getNextPos(timestamp, plainTicks) {
     var startPlain = Object.keys(plainTicks)[0];
     var endPlain = Object.keys(plainTicks)[Object.keys(plainTicks).length - 1];
 
@@ -248,8 +247,8 @@ function getNextPos(timestamp, plainTicks){
         timestamp = endPlain;
     if (timestamp < startPlain)
         timestamp = startPlain;
-    if(plainTicks[timestamp] == undefined)
-        return plainTicks[getCloseTimestamo(timestamp,plainTicks)];
+    if (plainTicks[timestamp] == undefined)
+        return plainTicks[getCloseTimestamo(timestamp, plainTicks)];
     return plainTicks[timestamp];
 }
 
@@ -265,7 +264,7 @@ function stopPlayback() {
 
 // TODO - make in seperate class of date utils
 function dateStr(time) {
-    return new Date(time*1000).toDateString();
+    return new Date(time * 1000).toDateString();
 }
 
 function timeStr(time) {
@@ -288,7 +287,6 @@ function timeStr(time) {
 }
 
 
-
 function setCurrentTimestamp(time) {
     currentTimestamp = time;
 }
@@ -301,18 +299,26 @@ function initlize() {
 
 
 function updateD3() {
-    plain = svg.selectAll(".g-plain")
-        .data(plainsJSON.items)
-        .enter().append("g")
-        .attr("class", function (d) {
-            return "g-plain g-plain-" + d.id;
+    plainGroup = svg.selectAll(".g-plainGroup").data(plainsJSON.items);
+
+    plainGroup.enter().append("g")
+        .attr("class", function (plainItem) {
+            var ticks = [];
+            plainItem.path.coordinates.forEach(function (coordinateItem, j) {
+                // map between the timestamp to its coordinates TODO - add spped and angle
+                ticks[coordinateItem[3]] = [coordinateItem[0], coordinateItem[1]];
+            });
+            // TODO - need that d3 be synced with plainController - right now plain controller not remove the old plains
+            plainController[plainItem.id] = ticks;
+            return "g-plainGroup g-pcmlain-" + plainItem.id;
         }).on("click", onClick)
 
-    plain.append("path").attr({
+    plainGroup.append("path").attr({
         d: "m25.21488,3.93375c-0.44355,0 -0.84275,0.18332 -1.17933,0.51592c-0.33397,0.33267 -0.61055,0.80884 -0.84275,1.40377c-0.45922,1.18911 -0.74362,2.85964 -0.89755,4.86085c-0.15655,1.99729 -0.18263,4.32223 -0.11741,6.81118c-5.51835,2.26427 -16.7116,6.93857 -17.60916,7.98223c-1.19759,1.38937 -0.81143,2.98095 -0.32874,4.03902l18.39971,-3.74549c0.38616,4.88048 0.94192,9.7138 1.42461,13.50099c-1.80032,0.52703 -5.1609,1.56679 -5.85232,2.21255c-0.95496,0.88711 -0.95496,3.75718 -0.95496,3.75718l7.53,-0.61316c0.17743,1.23545 0.28701,1.95767 0.28701,1.95767l0.01304,0.06557l0.06002,0l0.13829,0l0.0574,0l0.01043,-0.06557c0,0 0.11218,-0.72222 0.28961,-1.95767l7.53164,0.61316c0,0 0,-2.87006 -0.95496,-3.75718c-0.69044,-0.64577 -4.05363,-1.68813 -5.85133,-2.21516c0.48009,-3.77545 1.03061,-8.58921 1.42198,-13.45404l18.18207,3.70115c0.48009,-1.05806 0.86881,-2.64965 -0.32617,-4.03902c-0.88969,-1.03062 -11.81147,-5.60054 -17.39409,-7.89352c0.06524,-2.52287 0.04175,-4.88024 -0.1148,-6.89989l0,-0.00476c-0.15655,-1.99844 -0.44094,-3.6683 -0.90277,-4.8561c-0.22699,-0.59493 -0.50356,-1.07111 -0.83754,-1.40377c-0.33658,-0.3326 -0.73578,-0.51592 -1.18194,-0.51592l0,0l-0.00001,0l0,0z",
-        'stroke-width': 0}).attr('fill', PLAIN_COLOR).attr("transform", "scale(0.4)");
+        'stroke-width': 0
+    }).attr('fill', PLAIN_COLOR).attr("transform", "scale(0.4)");
 
-    // plain.exit().remove();
+    plainGroup.exit().remove();
 }
 
 function onClick(d) {
@@ -322,16 +328,16 @@ function onClick(d) {
 
 map.on('click', function (e) {
 
-    // if plain not selected remove the color
+    // if plainGroup not selected remove the color
     if (plainSelected == false)
-        svg.select(".g-plain-" + selectedPlainId).selectAll("path").attr("fill", PLAIN_COLOR);
+        svg.select(".g-plainGroup-" + selectedPlainId).selectAll("path").attr("fill", PLAIN_COLOR);
     plainSelected = false;
 });
 
 function selectPlain(id) {
-    svg.select(".g-plain-" + selectedPlainId).selectAll("path").attr("fill", PLAIN_COLOR);
+    svg.select(".g-plainGroup-" + selectedPlainId).selectAll("path").attr("fill", PLAIN_COLOR);
     selectedPlainId = id;
-    svg.select(".g-plain-" + id).selectAll("path").attr("fill", SELECTED_PLAIN_COLOR);
+    svg.select(".g-plainGroup-" + id).selectAll("path").attr("fill", SELECTED_PLAIN_COLOR);
     $('#flightId').after("<li>" + id + "</li>");
     plainSelected = true;
 }
@@ -339,15 +345,15 @@ function selectPlain(id) {
 
 // TODO - need to be in service
 function initData() {
-    $.getJSON("data/initial/" + currentTimestamp, function(result){
+    $.getJSON("data/initial/" + currentTimestamp, function (result) {
         plainsJSON = result;
-        initTicks();
+        // initTicks();
         initlize();
     });
 }
 
 function updateData() {
-    $.getJSON("data/initial/" + currentTimestamp, function(result){
+    $.getJSON("data/initial/" + currentTimestamp, function (result) {
         plainsJSON = result;
         updateD3();
         updateProjection();
@@ -359,7 +365,7 @@ function getPlainController() {
     return plainController;
 }
 
-function initTicks(){
+function initTicks() {
 
     plainsJSON.items.forEach(function (plainItem, i) {
 
@@ -388,30 +394,36 @@ map.on("render", function () {
 
 function updateProjection() {
 
-    if (typeof plain != 'undefined') {
-        plain.attr("transform", function (d) {
-            // in case the plain path is over
+    if (typeof plainGroup != 'undefined') {
+        plainGroup.attr("transform", function (d) {
+            // in case the plainGroup path is over
             if (d.endTime < currentTimestamp) {
-        //         // TODO - hide plain from map or remove
-        //         svg.select(".g-plain-" + d.id).remove();
-        //         console.log("remove path plain " + d.id);
+                //         // TODO - hide plainGroup from map or remove
+                //         svg.select(".g-plainGroup-" + d.id).remove();
+                //         console.log("remove path plainGroup " + d.id);
                 return;
             }
 
-            if(svg.select(".g-plain-" + d.id)[0] == null)
-            {
-                console.log("create plain d3 " + d.id);
-                plain.append("path").attr({
+            if (svg.select(".g-plainGroup-" + d.id)[0] == null) {
+                console.log("create plainGroup d3 " + d.id);
+                plainGroup.append("path").attr({
                     d: "m25.21488,3.93375c-0.44355,0 -0.84275,0.18332 -1.17933,0.51592c-0.33397,0.33267 -0.61055,0.80884 -0.84275,1.40377c-0.45922,1.18911 -0.74362,2.85964 -0.89755,4.86085c-0.15655,1.99729 -0.18263,4.32223 -0.11741,6.81118c-5.51835,2.26427 -16.7116,6.93857 -17.60916,7.98223c-1.19759,1.38937 -0.81143,2.98095 -0.32874,4.03902l18.39971,-3.74549c0.38616,4.88048 0.94192,9.7138 1.42461,13.50099c-1.80032,0.52703 -5.1609,1.56679 -5.85232,2.21255c-0.95496,0.88711 -0.95496,3.75718 -0.95496,3.75718l7.53,-0.61316c0.17743,1.23545 0.28701,1.95767 0.28701,1.95767l0.01304,0.06557l0.06002,0l0.13829,0l0.0574,0l0.01043,-0.06557c0,0 0.11218,-0.72222 0.28961,-1.95767l7.53164,0.61316c0,0 0,-2.87006 -0.95496,-3.75718c-0.69044,-0.64577 -4.05363,-1.68813 -5.85133,-2.21516c0.48009,-3.77545 1.03061,-8.58921 1.42198,-13.45404l18.18207,3.70115c0.48009,-1.05806 0.86881,-2.64965 -0.32617,-4.03902c-0.88969,-1.03062 -11.81147,-5.60054 -17.39409,-7.89352c0.06524,-2.52287 0.04175,-4.88024 -0.1148,-6.89989l0,-0.00476c-0.15655,-1.99844 -0.44094,-3.6683 -0.90277,-4.8561c-0.22699,-0.59493 -0.50356,-1.07111 -0.83754,-1.40377c-0.33658,-0.3326 -0.73578,-0.51592 -1.18194,-0.51592l0,0l-0.00001,0l0,0z",
-                    'stroke-width': 0}).attr('fill', PLAIN_COLOR).attr("transform", "scale(0.4)");
+                    'stroke-width': 0
+                }).attr('fill', PLAIN_COLOR).attr("transform", "scale(0.4)");
             }
 
-            var loc = plainController[d.id][currentTimestamp];
+
+            if(plainController[d.id] == undefined){
+                console.log("plainController is null " + d.id);
+return;}
+            else {
+                var loc = plainController[d.id][currentTimestamp];
+
             // var loc = [d.path.coordinates[sliderIndex][0], d.path.coordinates[sliderIndex][1]];
             if (loc == undefined)
-                loc = plainController[d.id][getCloseTimestamo(currentTimestamp,plainController[d.id])];
+                loc = plainController[d.id][getCloseTimestamo(currentTimestamp, plainController[d.id])];
             // pos = map.project(loc);
-            var pos = mapboxProjection(loc);
+            var pos = mapboxProjection(loc);}
             return "translate(" + pos[0] + "," + pos[1] + ")";
         });
     }
@@ -424,11 +436,10 @@ function mapboxProjection(lonlat) {
 }
 
 
-
 function sliderChanged(sliderIndex) {
     console.log(sliderIndex);
-    plain.attr("transform", function (d) {
-        // in case the plain path is over
+    plainGroup.attr("transform", function (d) {
+        // in case the plainGroup path is over
         if (d.path.coordinates.length <= sliderIndex) {
             // TODO - remove d.id from data
             return;
@@ -442,7 +453,7 @@ function sliderChanged(sliderIndex) {
 function movePlain(id, newLoc) {
 
     pos = mapboxProjection(newLoc);
-    svg.select(".g-plain-" + id).selectAll("path")
+    svg.select(".g-plainGroup-" + id).selectAll("path")
         .attr("transform", "translate(" + pos[0] + "," + pos[1] + ") scale(0.4)");
 }
 
@@ -486,7 +497,7 @@ function movePlain(id, newLoc) {
 function getCloseTimestamo(timestamp, plainTicks) {
     // for (first in obj) break;
     var curr = Object.keys(plainTicks)[0];
-    for(currTime in plainTicks){
+    for (currTime in plainTicks) {
         if (Math.abs(timestamp - currTime) < Math.abs(timestamp - curr))
             curr = currTime;
     }
